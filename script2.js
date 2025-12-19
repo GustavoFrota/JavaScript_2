9b11502f039fc0ca58a97fee4754cdb300826540
GustavoFrota <gustavofro90@gmail.com>
2025-12-19 08:26:00 -0300

    Adicionando idade na função

--- script2.js
+++ script2.js
@@ -1,6 +1,7 @@
 function agir() {
     let nome = document.getElementById("nome").value
     let sobrenome = document.getElementById("sobrenome").value
+    let idade = document.getElementById("idade").value
 
-    document.getElementById("Resultado").innerHTML = `Olá, nome é ${nome} ${sobrenome}`
+    document.getElementById("Resultado").innerHTML = `Olá, meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos.`
 }
